e5c31d1914dbc0fec863a606021cadc6b7b02e69
no <no@techvsi.com>
2019-10-04 01:45:38 +0700

    Update logo link Landing page

--- code/index.php
+++ code/index.php
@@ -29,10 +29,10 @@
 </head>
 <body>
 <div class="page-to-top">
-	<img src="images/register.png" alt=""  class="dt-block" />
+	<a href="https://kanagawaselect8.jp/contact/"><img src="images/register.png" alt=""  class="dt-block" /></a>
 	<img src="images/page-top.png" alt=""  class="back-to-top"/>
 	<div class="banner mb-block">
-		<img src="images/banner-pink.png" alt=""/>
+			<a href="https://kanagawaselect8.jp/contact/"><img src="images/banner-pink.png" alt=""/></a>
 	</div>
 </div>
 <section>
