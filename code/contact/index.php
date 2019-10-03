e72510aea16c6c96fca4cf6bb17b9146b38c7058
Thang <phamdinhthang1412@gmail.com>
2019-10-03 14:48:59 +0700

    name input

--- code/contact/index.php
+++ code/contact/index.php
@@ -71,7 +71,7 @@
         <div class="row">
           <div class="contentBody">
             <form action="#" class="orderForm form-inline" id="orderForm" method="POST">
-              <div class="row lineForm">
+              <div class="row lineForm form1">
                 <div class="col-md-2">
                   <h2>
                     <ruby>お名前 <rt>必須 </rt>
@@ -81,15 +81,15 @@
                 </div>
                 <div class="form-group col-md-5">
                   <label for="text">姓</label>
-                  <input type="text" class="form-control" id="text" />
+                  <input type="text" class="form-control" name=input1 />
                 </div>
                 <div class="form-group col-md-5">
                   <label for="text">名</label>
-                  <input type="text" class="form-control" id="text" />
+                  <input type="text" class="form-control" name=input12 />
                 </div>
               </div>
 
-              <div class="row lineForm">
+              <div class="row lineForm form2">
                 <div class="col-md-2">
                   <h2>
                     <ruby>フリガナ <rt>必須 </rt>
@@ -99,11 +99,11 @@
                 </div>
                 <div class="form-group col-md-5">
                   <label for="text">セイ</label>
-                  <input type="text" class="form-control" id="text" />
+                  <input type="text" class="form-control" name=input2 />
                 </div>
                 <div class="form-group col-md-5">
                   <label for="text">メイ</label>
-                  <input type="text" class="form-control" id="text" />
+                  <input type="text" class="form-control" name=input21 />
                 </div>
               </div>
 
@@ -117,9 +117,9 @@
                     <span>＊全角カタカナ</span>
                   </h2>
                 </div>
-                <div class="form-group col-md-5">
+                <div class="form-group col-md-5 form3">
                   <label for="text">〒</label>
-                  <input type="text" class="form-control" id="text" />
+                  <input type="text" class="form-control" name="input3" />
                 </div>
                 <div class="form-group col-md-5 dropText">
                   ＊ハイフンなしで入力
@@ -127,10 +127,10 @@
               </div>
               <div class="row mT15">
                 <div class="col-md-2"></div>
-                <div class="form-group col-md-5">
+                <div class="form-group col-md-5 form4">
                   <label for="text" class="w-100">都道府県名</label>
                   <div class="wrapSelect">
-                    <select class="selectForm" name="" id="">
+                    <select class="selectForm" name="select4" id="">
                       <option value="test">test</option>
                       <option value="test1">test1</option>
                     </select>
@@ -140,17 +140,17 @@
               </div>
               <div class="row mT15">
                 <div class="col-md-2"></div>
-                <div class="form-group col-md-7">
+                <div class="form-group col-md-7 form5">
                   <label for="text" class="w-100">市町村名</label>
-                  <input type="text" class="form-control w-100" id="text" />
+                  <input type="text" class="form-control w-100" name="input5"/>
                 </div>
                 <div class="form-group col-md-3"></div>
               </div>
               <div class="row lineForm mT15">
                 <div class="col-md-2"></div>
-                <div class="form-group col-md-7">
+                <div class="form-group col-md-7 form6">
                   <label for="text" class="w-100">番地・マンション名・部屋番号</label>
-                  <input type="text" class="form-control w-100" id="text" />
+                  <input type="text" class="form-control w-100" name="input6"/>
                 </div>
                 <div class="form-group col-md-3"></div>
               </div>
@@ -177,8 +177,8 @@
                     </ruby>
                   </h2>
                 </div>
-                <div class="form-group col-md-7">
-                  <input type="text" class="form-control w-100" id="text" />
+                <div class="form-group col-md-7 form7">
+                  <input type="text" class="form-control w-100" name="input7"/>
                 </div>
                 <div class="form-group col-md-3"></div>
               </div>
@@ -190,19 +190,19 @@
                     </ruby>
                   </h2>
                 </div>
-                <div class="form-group col-md-4">
+                <div class="form-group col-md-4 form8">
                   <label class="double" for="text">大人</label>
                   <div class="wrapSelect small">
-                    <select class="selectForm" name="" id="">
+                    <select class="selectForm" name="select8">
                       <option value="test">test</option>
                       <option value="test1">test1</option>
                     </select>
                   </div>
                 </div>
-                <div class="form-group col-md-4">
+                <div class="form-group col-md-4 form9">
                   <label class="double" for="text">こども</label>
                   <div class="wrapSelect small">
-                    <select class="selectForm" name="" id="">
+                    <select class="selectForm" name="select9" >
                       <option value="test">test</option>
                       <option value="test1">test1</option>
                     </select>
@@ -230,7 +230,7 @@
                 </div>
                 <div class="col-md-2"></div>
               </div>
-              <div class="row lineForm mT15">
+              <div class="row lineForm mT15 form10">
                 <div class="col-md-2">
                   <h2>
                     <ruby>興味のある セミナー <rt>必須 </rt>
@@ -246,40 +246,40 @@
                       メジャー8社の設計者によるトークバトル
                       広さに頼らない「心地よい住まい」のつくり方
                       安心安全に暮らす 神奈川県の防災対策と対処法 「美人収納」
-                      捨てるが苦手な人も<input type="checkbox" value="" /><span class="checkmark"></span></label>
+                      捨てるが苦手な人も<input type="checkbox" value="" name="input10"/><span class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
-                    <label class="unset">広さに頼らない「心地よい住まい」のつくり方<input type="checkbox" value="" /><span
+                    <label class="unset">広さに頼らない「心地よい住まい」のつくり方<input type="checkbox" value="" name="input101" /><span
                         class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
-                    <label class="unset">安心安全に暮らす 神奈川県の防災対策と対処法<input type="checkbox" value="" /><span
+                    <label class="unset">安心安全に暮らす 神奈川県の防災対策と対処法<input type="checkbox" value="" name="input102"/><span
                         class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
-                    <label class="unset">「美人収納」 捨てるが苦手な人もできる仕分け術<input type="checkbox" value="" /><span
+                    <label class="unset">「美人収納」 捨てるが苦手な人もできる仕分け術<input type="checkbox" value="" name="input103"/><span
                         class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
-                    <label class="unset">元号、消費税の変化に伴い、抑えておきたいお金の話！<input type="checkbox" value="" /><span
+                    <label class="unset">元号、消費税の変化に伴い、抑えておきたいお金の話！<input type="checkbox" value="" name="input104"/><span
                         class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
                     <label class="unset">実質金利ゼロ!?
-                      住宅ローン&家計で2,000万節約するマル秘テクニック<input type="checkbox" value="" /><span
+                      住宅ローン&家計で2,000万節約するマル秘テクニック<input type="checkbox" value="" name="input105"/><span
                         class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
-                    <label class="unset">災害に強い安心・安全な住まいとは<input type="checkbox" value="" /><span
+                    <label class="unset">災害に強い安心・安全な住まいとは<input type="checkbox" value="" name="input105"/><span
                         class="checkmark"></span></label>
                   </div>
                   <div class="wrapCheckmart">
-                    <label class="unset">相続・贈与時の お得な減税制度とは<input type="checkbox" value="" /><span
+                    <label class="unset">相続・贈与時の お得な減税制度とは<input type="checkbox" value="" name="input106"/><span
                         class="checkmark"></span></label>
                   </div>
                 </div>
               </div>
-              <div class="row lineForm mT15">
+              <div class="row lineForm mT15 form11">
                 <div class="col-md-2">
                   <h2>
                     <ruby>託児所利用 <rt class="brown">任意 </rt>
@@ -290,7 +290,7 @@
                 <div class="col-md-10">
                   <div class="lineFormchild">
                     <div class="wrapCheckmart">
-                      <label class="unset">利用する<input type="checkbox" value="" /><span class="checkmark"></span></label>
+                      <label class="unset">利用する<input type="checkbox" value="" name="input11" /><span class="checkmark"></span></label>
                     </div>
                   </div>
                   <div class="row ml--15">
@@ -298,7 +298,7 @@
                       <div class="form-group col-md-4">
                         <label class="w-100" for="text">▽ご利用人数</label>
                         <div class="wrapSelect small">
-                          <select class="selectForm" name="" id="">
+                          <select class="selectForm" name="select111">
                             <option value="選択してください" selected >選択してください</option>
                             <option value="1人">1人</option>
                             <option value="2人">2人</option>
@@ -315,7 +315,7 @@
                       <div class="form-group col-md-6">
                         <label class="w-100" for="text">▽ご希望の時間帯</label>
                         <div class="wrapSelect large">
-                          <select class="selectForm" name="" id="">
+                          <select class="selectForm" name="select112">
                             <option value="選択してください">選択してください</option>
                             <option value="午前">午前</option>
                           </select>
@@ -327,7 +327,7 @@
                 </div>
               </div>
 
-              <div class="row mT15">
+              <div class="row mT15 form12">
                 <div class="col-md-2">
                   <h2>
                     <ruby>参加メーカーとの 個別相談希望
@@ -341,7 +341,7 @@
                   <div class="row mT20 ml--15">
                     <div class="form-group col-md-4">
                       <div class="wrapCheckmart">
-                        <label class="unset">へーベルハウス<input type="checkbox" value="" /><span
+                        <label class="unset">へーベルハウス<input type="checkbox" value="" name="input12" /><span
                             class="checkmark"></span></label>
                       </div>
                     </div>
@@ -350,7 +350,7 @@
                     <div class="form-group col-md-8">
                       <label class="double" for="text">ご希望の時間帯</label>
                       <div class="wrapSelect large">
-                        <select class="selectForm" name="" id="">
+                        <select class="selectForm" name="select121" >
                           <option value="選択してください">選択してください</option>
                           <option value="11/16 10時半〜">11/16 10時半〜</option>
                           <option value="11/16 10時半〜">11/16 11時半〜</option>
