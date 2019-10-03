ab1ae947d2c5fb51999e6ce5b3477248d94af20b
Thang <phamdinhthang1412@gmail.com>
2019-10-04 01:31:49 +0700

    date

--- code/contact/contact_send.php
+++ code/contact/contact_send.php
@@ -61,30 +61,6 @@ $select12_8= ! empty( $_POST['select12_8'] ) ? $_POST['select12_8'] : '';
 
 
 
-$mail = new PHPMailer();
-
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-$mail->IsSMTP(); // enable SMTP
-
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "xxxxxx";
-$mail->Password = "xxxx";
-$mail->SetFrom("xxxxxx@xxxxx.com");
-$mail->Subject = "Test";
-$mail->Body = "hello";
-$mail->AddAddress("xxxxxx@xxxxx.com");
-
-if(!$mail->Send()) {
-	echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-	echo "Message has been sent";
-}
-
 // ホームページ運営者に返信するメール
 mb_language( "Japanese" );
 mb_internal_encoding( "UTF-8" );
