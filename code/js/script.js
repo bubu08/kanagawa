2ee1d9571e2cb7b38670984ef05b2867f29f931c
thangGuest <thangpd.slz@gmail.com>
2019-10-04 19:55:02 +0900

    guest update

--- code/js/script.js
+++ code/js/script.js
@@ -1,14 +1,15 @@
 jQuery(document).ready(function($){
     var btn = $('.page-to-top');
+    btn.addClass('show');
 
-    $(window).scroll(function() {
-        console.log('test');
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
-    } else {
-        btn.removeClass('show');
-    }
-    });
+    // $(window).scroll(function() {
+    //     console.log('test');
+    // if ($(window).scrollTop() >= 0) {
+    //     btn.addClass('show');
+    // } else {
+    //     btn.removeClass('show');
+    // }
+    // });
 
     $('.back-to-top').on('click', function(e) {
     e.preventDefault();
